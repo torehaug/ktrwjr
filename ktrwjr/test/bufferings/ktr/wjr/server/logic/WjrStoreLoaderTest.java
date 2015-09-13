c96a6c19b2382e804cb50185bf34030bd8f561b1
Tore Haugland <tore.haugland@gmail.com>
2015-09-13 16:50:59 +0200

    removed AppEngineUtil constants

--- ktrwjr/test/bufferings/ktr/wjr/server/logic/WjrStoreLoaderTest.java
+++ ktrwjr/test/bufferings/ktr/wjr/server/logic/WjrStoreLoaderTest.java
@@ -15,152 +15,103 @@
  */
 package bufferings.ktr.wjr.server.logic;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 
 import org.junit.Test;
 
 import bufferings.ktr.wjr.server.fortest.ForTest;
-import bufferings.ktr.wjr.server.util.AppEngineUtil;
 import bufferings.ktr.wjr.shared.model.WjrClassItem;
 import bufferings.ktr.wjr.shared.model.WjrStore;
 
 public class WjrStoreLoaderTest {
 
-  private WjrStoreLoader storeLoader = new WjrStoreLoader() {
-    @Override
-    protected void checkAndStoreTestClass(WjrStore store, Class<?> clazz) {
-      store.addClassItem(new WjrClassItem(clazz.getName()));
-    }
-  };
+	private WjrStoreLoader storeLoader = new WjrStoreLoader() {
+		@Override
+		protected void checkAndStoreTestClass(WjrStore store, Class<?> clazz) {
+			store.addClassItem(new WjrClassItem(clazz.getName()));
+		}
+	};
 
-  @Test(expected = NullPointerException.class)
-  public void loadWjrStore_WillThrowNPE_WhenSearchRootDirPathIsNull() {
-    storeLoader.loadWjrStore(null);
-  }
+	@Test(expected = NullPointerException.class)
+	public void loadWjrStore_WillThrowNPE_WhenSearchRootDirPathIsNull() {
+		storeLoader.loadWjrStore(null);
+	}
 
-  @Test(expected = IllegalArgumentException.class)
-  public void loadWjrStore_WillThrowIAE_WhenSearchRootDirPathDoesNotExist() {
-    if (AppEngineUtil.isServer()) {
-      storeLoader.loadWjrStore("NOTEXIST-WEB-INF/web.xml");
-    } else {
-      storeLoader.loadWjrStore("war/NOTEXIST-WEB-INF/web.xml");
-    }
-  }
+	@Test(expected = IllegalArgumentException.class)
+	public void loadWjrStore_WillThrowIAE_WhenSearchRootDirPathDoesNotExist() {
+		storeLoader.loadWjrStore("war/NOTEXIST-WEB-INF/web.xml");
+	}
 
-  @Test(expected = IllegalArgumentException.class)
-  public void loadWjrStore_WillThrowIAE_WhenSearchRootDirPathIsNotDir() {
-    if (AppEngineUtil.isServer()) {
-      storeLoader.loadWjrStore("WEB-INF/web.xml");
-    } else {
-      storeLoader.loadWjrStore("war/WEB-INF/web.xml");
-    }
-  }
+	@Test(expected = IllegalArgumentException.class)
+	public void loadWjrStore_WillThrowIAE_WhenSearchRootDirPathIsNotDir() {
+		storeLoader.loadWjrStore("war/WEB-INF/web.xml");
+	}
 
-  @Test
-  public void loadWjrStore_CanLoadWjrStore() {
-    WjrStore store;
-    if (AppEngineUtil.isServer()) {
-      store = storeLoader.loadWjrStore("WEB-INF/classes");
-    } else {
-      store = storeLoader.loadWjrStore("war/WEB-INF/classes");
-    }
-    for (WjrClassItem item : store.getClassItems()) {
-      if (item.getClassName().equals(ForTest.class.getName())) {
-        return;
-      }
-    }
-    fail();
-  }
+	@Test
+	public void loadWjrStore_CanLoadWjrStore() {
+		WjrStore store;
+		store = storeLoader.loadWjrStore("war/WEB-INF/classes");
+		for (WjrClassItem item : store.getClassItems()) {
+			if (item.getClassName().equals(ForTest.class.getName())) {
+				return;
+			}
+		}
+		fail();
+	}
 
-  @Test
-  public void findAndStoreTests_CanFindAndStoreTests() {
-    WjrStore store = new WjrStore();
-    if (AppEngineUtil.isServer()) {
-      File parentDir = new File("WEB-INF/classes");
-      storeLoader.findAndStoreTests(store, "WEB-INF/classes", parentDir);
-    } else {
-      File parentDir = new File("war/WEB-INF/classes");
-      storeLoader.findAndStoreTests(store, "war/WEB-INF/classes", parentDir);
-    }
-    for (WjrClassItem item : store.getClassItems()) {
-      if (item.getClassName().equals(ForTest.class.getName())) {
-        return;
-      }
-    }
-    fail();
-  }
+	@Test
+	public void findAndStoreTests_CanFindAndStoreTests() {
+		WjrStore store = new WjrStore();
+		File parentDir = new File("war/WEB-INF/classes");
+		storeLoader.findAndStoreTests(store, "war/WEB-INF/classes", parentDir);
+		for (WjrClassItem item : store.getClassItems()) {
+			if (item.getClassName().equals(ForTest.class.getName())) {
+				return;
+			}
+		}
+		fail();
+	}
 
-  @Test
-  public void listClassFiles_CanListClassFiles() {
-    File parentDir;
-    if (AppEngineUtil.isServer()) {
-      parentDir = new File("WEB-INF/classes/bufferings/ktr/wjr/server/fortest");
-    } else {
-      parentDir =
-        new File("war/WEB-INF/classes/bufferings/ktr/wjr/server/fortest");
-    }
-    File[] children = storeLoader.listClassFiles(parentDir);
-    assertThat(children.length, is(8));
-    // ForTest,ForTestInnerStatic,ForTestInnerNotStatic,ForTestInherit
-    for (File child : children) {
-      assertThat(child.isFile(), is(true));
-    }
-  }
+	@Test
+	public void listClassFiles_CanListClassFiles() {
+		File parentDir;
+		parentDir = new File("war/WEB-INF/classes/bufferings/ktr/wjr/server/fortest");
+		File[] children = storeLoader.listClassFiles(parentDir);
+		assertThat(children.length, is(8));
+		// ForTest,ForTestInnerStatic,ForTestInnerNotStatic,ForTestInherit
+		for (File child : children) {
+			assertThat(child.isFile(), is(true));
+		}
+	}
 
-  @Test
-  public void listDirectories_CanListDirectories() {
-    File parentDir;
-    if (AppEngineUtil.isServer()) {
-      parentDir = new File("WEB-INF/classes/bufferings/ktr/wjr/server");
-    } else {
-      parentDir = new File("war/WEB-INF/classes/bufferings/ktr/wjr/server");
-    }
-    File[] children = storeLoader.listDirectories(parentDir);
-    assertThat(children.length, is(4));
-    assertThat(children[0].isDirectory(), is(true));
-    assertThat(children[1].isDirectory(), is(true));
-    assertThat(children[2].isDirectory(), is(true));
-    assertThat(children[3].isDirectory(), is(true));
-  }
+	@Test
+	public void listDirectories_CanListDirectories() {
+		File parentDir;
+		parentDir = new File("war/WEB-INF/classes/bufferings/ktr/wjr/server");
+		File[] children = storeLoader.listDirectories(parentDir);
+		assertThat(children.length, is(4));
+		assertThat(children[0].isDirectory(), is(true));
+		assertThat(children[1].isDirectory(), is(true));
+		assertThat(children[2].isDirectory(), is(true));
+		assertThat(children[3].isDirectory(), is(true));
+	}
 
-  @Test
-  public void getClassNameFromClassFile_CanGetName() {
-    if (AppEngineUtil.isServer()) {
-      File classFile =
-        new File(
-          "WEB-INF/classes/bufferings/ktr/wjr/server/fortest/ForTest.class");
-      String ret =
-        storeLoader.getClassNameFromClassFile("WEB-INF/classes", classFile);
-      assertThat(ret, is(ForTest.class.getName()));
-    } else {
-      File classFile =
-        new File(
-          "war/WEB-INF/classes/bufferings/ktr/wjr/server/fortest/ForTest.class");
-      String ret =
-        storeLoader.getClassNameFromClassFile("war/WEB-INF/classes", classFile);
-      assertThat(ret, is(ForTest.class.getName()));
-    }
-  }
+	@Test
+	public void getClassNameFromClassFile_CanGetName() {
+		File classFile = new File("war/WEB-INF/classes/bufferings/ktr/wjr/server/fortest/ForTest.class");
+		String ret = storeLoader.getClassNameFromClassFile("war/WEB-INF/classes", classFile);
+		assertThat(ret, is(ForTest.class.getName()));
+	}
 
-  @Test
-  public void getClassNameFromClassFile_CanGetName_WithInnerStaticClass() {
-    if (AppEngineUtil.isServer()) {
-      File classFile =
-        new File(
-          "WEB-INF/classes/bufferings/ktr/wjr/server/fortest/ForTest$ForTestInnerStatic.class");
-      String ret =
-        storeLoader.getClassNameFromClassFile("WEB-INF/classes", classFile);
-      assertThat(ret, is(ForTest.ForTestInnerStatic.class.getName()));
-    } else {
-      File classFile =
-        new File(
-          "war/WEB-INF/classes/bufferings/ktr/wjr/server/fortest/ForTest$ForTestInnerStatic.class");
-      String ret =
-        storeLoader.getClassNameFromClassFile("war/WEB-INF/classes", classFile);
-      assertThat(ret, is(ForTest.ForTestInnerStatic.class.getName()));
-    }
-  }
+	@Test
+	public void getClassNameFromClassFile_CanGetName_WithInnerStaticClass() {
+		File classFile = new File(
+				"war/WEB-INF/classes/bufferings/ktr/wjr/server/fortest/ForTest$ForTestInnerStatic.class");
+		String ret = storeLoader.getClassNameFromClassFile("war/WEB-INF/classes", classFile);
+		assertThat(ret, is(ForTest.ForTestInnerStatic.class.getName()));
+	}
 }
